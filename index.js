a59234526dfb6aa596f63c4fe97db952a696c47e
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-10-29 13:01:13 -0400

    wip

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require("dotenv").config();
 const server = require("./api/server.js");
 
-const port = 5000;
+// const port = 5000; // ONLY LOCAL
+// process.env.PORT; // ONLY HEROKU
+
+const port = process.env.PORT;
+const secret = process.env.SECRET_THING;
+
+console.log(port, secret);
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
